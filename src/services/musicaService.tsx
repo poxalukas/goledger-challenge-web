13430b4ecba6f51b32d029fe5d472f1aa1825c3b
lucas-silva-aa <lllucaslacerda@gmail.com>
2024-12-08 17:56:31 -0300

    feat: criando services

---
+++ src/services/musicaService.tsx
@@ -0,0 +1,47 @@
+import { url } from "./url_api";
+
+const username = 'psAdmin';
+const password = 'goledger';
+
+export async function listarTodosSons() {
+  const body = {
+    query: {
+      selector: {
+        "@assetType": "song",
+      },
+    },
+  };
+
+  const headers = new Headers();
+  headers.set('Authorization', 'Basic ' + btoa(`${username}:${password}`));
+  headers.set('Content-Type', 'application/json');
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    const sortedData = result?.result?.sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    }) || [];
+
+    return sortedData;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}

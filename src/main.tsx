a43a228e5a59c55adf04b32dada052919fc4326c
lucas-silva-aa <lllucaslacerda@gmail.com>
2024-12-08 19:03:24 -0300

    feat:configuraçoes gerais

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Importe o BrowserRouter
+import './styles/tailwind.css';
+import { App } from './App.tsx';
+import { Sidebar } from './components/sidebar.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <div > 
+        <div >
+          <Sidebar />
+        </div>
+        <div> 
+          <App />
+        </div>
+      </div>
+    </BrowserRouter>
+  </StrictMode>
+);

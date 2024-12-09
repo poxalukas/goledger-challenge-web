744836dd1d61171afffb5d3bdc070d43b22c1b1a
lucas-silva-aa <130592625+lucas-silva-aa@users.noreply.github.com>
2024-12-09 13:53:01 -0300

    feat: melhorias no menu

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,26 +1,34 @@
-import { Link } from 'react-router-dom';
-import '../css/sidebar.css';
+import { Link } from "react-router-dom";
+import "../css/sidebar.css";
 
 export const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar-header">
-      </div>
-      <ul className="nav-list">
+      <ul className="nav-list flex space-x-6">
         <li className="nav-item">
-          <Link to="/home" className="link-style">Home</Link>
+          <Link to="/home" className="link-style">
+            Home
+          </Link>
         </li>
         <li className="nav-item">
-          <Link to="/artistas" className="link-style">Artistas</Link>
+          <Link to="/artist" className="link-style">
+            Artistas
+          </Link>
         </li>
         <li className="nav-item">
-          <Link to="/albums" className="link-style">Albums</Link>
+          <Link to="/albums" className="link-style">
+            Albums
+          </Link>
         </li>
         <li className="nav-item">
-          <Link to="/musicas" className="link-style">Musicas</Link>
+          <Link to="/musicas" className="link-style">
+            Músicas
+          </Link>
         </li>
         <li className="nav-item">
-          <Link to="/playlists" className="link-style">Playlist</Link>
+          <Link to="/playlists" className="link-style">
+            Playlist
+          </Link>
         </li>
       </ul>
     </div>
